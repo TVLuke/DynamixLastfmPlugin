f13c77c0516e4f63269d591daea288d49fbd7596
Lukas Ruge <ruge@itm.uni-luebeck.de>
2013-08-01 16:35:04 +0200

    plugin update

--- src/org/ambientdynamix/contextplugins/lastfm/CurrentSongContextInfo.java
+++ src/org/ambientdynamix/contextplugins/lastfm/CurrentSongContextInfo.java
@@ -5,24 +5,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.ambientdynamix.api.application.IContextInfo;
-import org.ambientdynamix.contextplugins.context.info.environment.ICurrentTimeContextInfo;
+import org.ambientdynamix.contextplugins.context.info.environment.ICurrentSongContextInfo;
 
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
 
-public class CurrentSongContextInfo implements IContextInfo, ICurrentTimeContextInfo
+public class CurrentSongContextInfo implements IContextInfo, ICurrentSongContextInfo
 {
 
 	private final String TAG = "TIMEPLUGIN";
 	
-	int year;
-	int month;
-	int dayofthemonth;
-	int houroftheday;
-	int minuteofthehour;
-	int secondoftheminute;
-	int dayoftheweek;
+	String title;
+	String artist;
+	int length;
+	String album;
+	String tags;
 	
 	public static Parcelable.Creator<CurrentSongContextInfo> CREATOR = new Parcelable.Creator<CurrentSongContextInfo>() 
 			{
@@ -39,26 +37,16 @@ public class CurrentSongContextInfo implements IContextInfo, ICurrentTimeContext
 		
 	CurrentSongContextInfo()
 	{
-		Log.d(TAG, "create Current Time");
-		Date d = new Date();
-		year=d.getYear()+1900;
-		month=d.getMonth();
-		dayofthemonth=d.getDate();
-		houroftheday=d.getHours();
-		minuteofthehour=d.getMinutes();
-		secondoftheminute=d.getSeconds();
-		dayoftheweek=d.getDay();
+
 	}
 	
 	public CurrentSongContextInfo(Parcel in) 
 	{
-		year=in.readInt();
-		month=in.readInt();
-		dayofthemonth=in.readInt();
-		houroftheday=in.readInt();
-		minuteofthehour=in.readInt();
-		secondoftheminute=in.readInt();
-		dayoftheweek=in.readInt();
+		String title = in.readString();
+		String artist = in.readString();
+		int length = in.readInt();
+		String album = in.readString();
+		String tags = in.readString();
 	}
 
 	@Override
@@ -77,13 +65,11 @@ public class CurrentSongContextInfo implements IContextInfo, ICurrentTimeContext
 	@Override
 	public void writeToParcel(Parcel out, int flags) 
 	{
-		out.writeInt(year);
-		out.writeInt(month);
-		out.writeInt(dayofthemonth);
-		out.writeInt(houroftheday);
-		out.writeInt(minuteofthehour);
-		out.writeInt(secondoftheminute);
-		out.writeInt(dayoftheweek);
+		out.writeString(title);
+		out.writeString(artist);
+		out.writeInt(length);
+		out.writeString(album);
+		out.writeString(tags);
 	}
 
 	@Override
@@ -104,36 +90,11 @@ public class CurrentSongContextInfo implements IContextInfo, ICurrentTimeContext
 		String result="";
 		if (format.equalsIgnoreCase("text/plain"))
 		{
-			String smonth=""+month;
-			String sday=""+dayofthemonth;
-			String shour=""+houroftheday;
-			String sminute=""+minuteofthehour;
-			String ssecond=""+secondoftheminute;
-			if(month<10)
-			{
-				smonth="0"+smonth;
-			}
-			if(dayofthemonth<10)
-			{
-				sday="0"+sday;
-			}
-			if(houroftheday<10)
-			{
-				shour="0"+shour;
-			}
-			if(minuteofthehour<10)
-			{
-				sminute="0"+sminute;
-			}
-			if(secondoftheminute<10)
-			{
-				ssecond="0"+ssecond;
-			}
-			return ""+year+"-"+smonth+"-"+sday+" "+shour+":"+sminute+":"+ssecond;
+			return "";
 		}
 		else if (format.equalsIgnoreCase("XML"))
 		{
-			return "<time><year>"+year+"</year></time>";
+			return "";
 		}
 		else if (format.equalsIgnoreCase("JSON"))
 		{
@@ -154,106 +115,20 @@ public class CurrentSongContextInfo implements IContextInfo, ICurrentTimeContext
 	}
 
 	@Override
-	public int dayOfMonth() 
+	public String getName() 
 	{
-		return month;
+		return title;
 	}
 
 	@Override
-	public int dayOfWeek() 
+	public String getArtist() 
 	{
-		return dayoftheweek;
+		return artist;
 	}
 
 	@Override
-	public int dayOfYear() 
+	public int getLength() 
 	{
-		return 0;
+		return length;
 	}
-
-	@Override
-	public int era() 
-	{
-		return 0;
-	}
-
-	@Override
-	public int hourOfDay() 
-	{
-		return houroftheday;
-	}
-
-	@Override
-	public int millisOfDay() 
-	{
-		return 0;
-	}
-
-	@Override
-	public int millisOfSecond() 
-	{
-		return 0;
-	}
-
-	@Override
-	public int minuteOfDay() 
-	{
-		if(houroftheday>0)
-		{
-			return minuteofthehour+(60*(houroftheday-1));
-		}
-		else
-		{
-			return minuteofthehour;
-		}
-	}
-
-	@Override
-	public int minuteOfHour() 
-	{
-		return minuteofthehour;
-	}
-
-	@Override
-	public int monthOfYear() 
-	{
-		return month;
-	}
-
-	@Override
-	public int secondOfDay() 
-	{
-		return 0;
-	}
-
-	@Override
-	public int secondOfMinute() 
-	{
-		return secondoftheminute;
-	}
-
-	@Override
-	public int weekOfWeekyear() 
-	{
-		return 0;
-	}
-
-	@Override
-	public int year() 
-	{
-		return year;
-	}
-
-	@Override
-	public int yearOfCentury() 
-	{
-		return year%100;
-	}
-
-	@Override
-	public int yearOfEra() 
-	{
-		return 0;
-	}
-
 }
