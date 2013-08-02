6aebda161d013c3e27ac06432db5dea3eb924957
Lukas Ruge <ruge@itm.uni-luebeck.de>
2013-08-02 10:11:12 +0200

    update

--- src/org/ambientdynamix/contextplugins/lastfm/Constants.java
+++ src/org/ambientdynamix/contextplugins/lastfm/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
 	public static final String API_KEY="6635bf515b7cee1e9a6bb30a67339a9d";
 	public static final String PREFS = "lastfmdynamixpluginprefs";
+	public static final String USERNAME = "lastfmusername";
 }
